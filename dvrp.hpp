42f134dcef0a64062a4123d83b2e07a51586d0c1
pooriataj <pouriyatajmehrabi1381@gmail.com>
2023-06-02 02:08:10 +0330

    Add DVRCP and a launch.json that allows make file debugging

---
+++ dvrp.hpp
@@ -0,0 +1,21 @@
+#ifndef DVRPHPP
+#define DVRPHPP
+#include <vector>
+#include "topology.hpp"
+using namespace std;
+#include <iostream>
+#define INF __INT_MAX__
+using namespace std;
+
+class DVRP
+{
+public:
+    int node_count;
+    int source;
+    Topology *topology;
+    vector<int> distances;
+    vector<int> parents;
+    DVRP(int node_count, int source, Topology *topology);
+    void report();
+};
+#endif
